6633f65a30909b4ceb8586070588649509fda21a
Jahid Chowdhury <mdjahidchowdhury96@gmai.com>
2023-02-22 14:34:38 +0600

    first commit

---
+++ calculate.js
@@ -0,0 +1,44 @@
+const blog = document.getElementById('my-blog').addEventListener('click',function(){
+   window.open('blog.html');
+}
+)
+const tribuj = document.getElementById('triangle-calculate').addEventListener('click',function(){
+    const heighte = document.getElementById('h').value;
+    const land = document.getElementById('b').value;
+    const trianglecalculation = 0.5* heighte*land;
+    const trianglearea = 'Tringle:'+trianglecalculation;
+
+})
+const rectangle = document.getElementById('rectangle-calculate').addEventListener('click',function(){
+    const wide = document.getElementById('w').value;
+    const lanth = document.getElementById('l').value;
+    const rectanglearea = wide*lanth;
+   
+})
+     const parallelogram = document.getElementById('parallelogram-calculation').addEventListener('click',function(){
+    const perawide = document.getElementById('pera-b').value;
+    const peralanth = document.getElementById('pera-h').value;
+    const parallelogramarea = perawide*peralanth;
+   
+})
+
+
+const rhombus = document.getElementById('rhombus').addEventListener('click',function(){
+    const rhombuswide = document.getElementById('d1').value;
+    const rhombuslanth = document.getElementById('d2').value;
+    const rhombusarea = 0.5*rhombuslanth*rhombuswide;
+   
+})
+
+const pentagon = document.getElementById('pentagon').addEventListener('click',function(){
+    const pentagonp = document.getElementById('pen-p').value;
+    const pentagonb = document.getElementById('pen-b').value;
+    const pentagonArea= 0.5*pentagonp*pentagonb;
+   
+})
+
+const ellipse = document.getElementById('ellipse').addEventListener('click',function(){
+    const ellipseA = document.getElementById('ab').value;
+    const ellipseB = document.getElementById('ba').value;
+    const ellipseArea= 3.14*ellipseA*ellipseB;
+})
